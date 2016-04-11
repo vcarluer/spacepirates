0cd5fa4179256e7141661e646b10d0530c427a84
Vincent Carluer <vcarluer@gmail.com>
2016-04-11 12:00:25 +0000

    title

--- spacepirates.js
+++ spacepirates.js
@@ -8,8 +8,37 @@ function ready(fn) {
 
 var spacepirates = {
     run: function() {
-        this.mainDiv = document.getElementById('app');
-        this.mainDiv.innerHTML = 'Hello pirate!';
+        var mainDiv = document.getElementById('app');
+        this.showTitle(mainDiv)
+    },
+    
+    showTitle: function(div) {
+        var container = document.createElement('container');
+        container.className = 'container';
+        div.appendChild(container);
+        
+        var titleDiv = document.createElement('div');
+        titleDiv.innerHTML = 'Space pirates';
+        titleDiv.className = 'title';
+        container.appendChild(titleDiv);
+        var subTitleDiv = document.createElement('div');
+        subTitleDiv.innerHTML = "All aboard!";
+        subTitleDiv.className = 'title';
+        container.appendChild(subTitleDiv);
+        
+        var self = this;
+        document.body.onclick = function() {
+            div.removeChild(container);
+            self.showCard(div, 'Intro');
+        }
+    },
+    
+    showCard: function(div, cardName) {
+        var container = document.createElement('container');
+        container.className = 'container';
+        div.appendChild(container);
+        
+        container.innerHTML = 'hoho';
     }
 };
 
